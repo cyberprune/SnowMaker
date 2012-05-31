fe2f0d28e1b9a5cd40ec62cdb2813f1e80fc33f1
Daniel Butler <daniel@cyberprune.com>
2012-05-31 22:52:18 +0930

    Updated MongoDB Driver and Cleanup
    - Updated MongoDB Driver
    - Renamed SnowMaker.Providers.MongoDB to SnowMaker.Data.MongoDB

---
+++ SnowMaker.Data.MongoDB/SnowflakeBlock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace SnowMaker.Data.MongoDB
+{
+    public class SnowflakeBlock
+    {
+        [BsonId]
+        public string Name { get; set; }
+
+        public string Data { get; set; }
+    }
+}
